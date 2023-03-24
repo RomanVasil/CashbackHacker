ffc4de61aedf3bca05217d910a3c58cba85916d7
RomanVasil <98211521+RomanVasil@users.noreply.github.com>
2023-03-24 14:10:24 +0400

    initial commit JUnit5

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remain1() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+        //System.out.println(actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void remain1000() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void remain1001() {
+
+           CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+      //  System.out.println(actual);
+        assertEquals(expected, actual);
+    }
+}
